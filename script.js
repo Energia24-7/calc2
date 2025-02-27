bdb20c7b90abe3b0b3fd50c3f5f3176377fe8ac6
Energia24-7 <fastindustrialsupply@gmail.com>
2025-02-27 10:36:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ async function findBestCapacity(lat, lon, consumoMensual, apiKey) {
                 const monthlyGen = data.outputs.ac_annual / 12;
                 agregarFilaTabla(capacity, monthlyGen);
                 
-                if (monthlyGen*1.05 >= consumoMensual) {
+                if (monthlyGen >= consumoMensual*1.05) {
                     bestCapacity = capacity;
                     break;
                 }
